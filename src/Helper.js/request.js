efb135e3439af425e4bd85bc6f79aa4e285d0598
Edmon <you@example.com>
2021-07-16 13:56:30 +0400

    feat(add): add request funtion

---
+++ src/Helper.js/request.js
@@ -0,0 +1,34 @@
+const main_url='http://localhost:3004'
+
+function request(url,options){
+    const config = {
+        method: options.method || "GET",
+        headers: {
+            "content-type": "application/json"
+        }
+    }
+    url = main_url + url;
+    if(options.body){
+        config.body = JSON.stringify(options.body)
+    }
+    if(options.query) {
+        url = url + query(options.query)
+    }
+    return fetch(url,config)
+    .then((res) => res.json())
+    .then((response) => {
+        if(response.error){
+            throw response.error
+        }
+        return response
+    })
+ }
+ const query = (objQuery) => {
+    let queryStr = "?"
+    Object.entries(objQuery).forEach((el) => {
+       queryStr += el[0] + "=" + el[1].toString() + "&"
+    })
+    return queryStr;
+ }
+
+ export default request
