da204fd9ad8b868e522ba265b60e58773f85fffe
Edmon <you@example.com>
2021-07-28 02:11:05 +0400

    refactor(): rewrite login class component to hook

--- src/Login/LoginHook.js
+++ src/Login/LoginHook.js
@@ -1,54 +1,115 @@
-import { useState} from "react";
-import { Link, useHistory} from "react-router-dom";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import request from "../Helper.js/request";
-import styles from "./Login.module.css"
+import {Avatar,Button,CssBaseline,TextField,FormControlLabel,Typography,Checkbox,Container} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 export default function LoginHook() {
+  const classes = useStyles();
   let history = useHistory();
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("");
   const emailHandler = (e) => {
-      setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
   const passwordHandler = (e) => {
-    setPassword(e.target.value)
-  }
+    setPassword(e.target.value);
+  };
   const checkUserInfo = (e) => {
     e.preventDefault();
-    request('/users',{
+    request("/users", {
       query: {
-        email
+        email,
+      },
+    }).then((user) => {
+      if (user.length > 0 && user[0].password === password) {
+        localStorage.setItem("id", user[0].id);
+        history.push("/userPage");
+      } else {
+        alert("email or password is incorrect");
       }
-    })
-    .then((user) => {
-        if(user.length > 0 && user[0].password === password){
-         localStorage.setItem('id',user[0].id)
-          history.push('/userPage')
-       }else{
-         alert("email or password is incorrect")
-       }
-    })
-  }
+    });
+  };
   return (
-    <div className={styles.form}>
-    <form onSubmit={(e) => checkUserInfo(e)}>
-        <label>
-            Email
-            <input type="email" placeholder="email" value={email} onChange={(e) => emailHandler(e)}/>
-        </label>
-        <label>
-            Password
-            <input type="password" placeholder="password" value={password} onChange={(e) => passwordHandler(e)}/>
-        </label>
-        <button>
-            Login
-        </button>
-    </form>
-    <p>
-    If you doesnt have account please   <Link to="/SignUp">Sign Up</Link> <br/>
-    <Link to="/"> Go home page </Link>
-  </p>
-</div>
-  )
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}></Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form
+          className={classes.form}
+          noValidate
+          onSubmit={(e) => checkUserInfo(e)}
+        >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => emailHandler(e)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => passwordHandler(e)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+        <p>
+          If you doesnt have account please <Link to="/SignUp">Sign Up</Link>{" "}
+          <br />
+          <Link to="/"> Go home page </Link>
+        </p>
+      </div>
+    </Container>
+  );
 }
