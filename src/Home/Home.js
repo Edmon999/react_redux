e1d59e0eb8ef27006c0f16fcdddf412994b3315f
Edmon <you@example.com>
2021-07-28 02:10:18 +0400

    feat(add): make styles

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,17 +1,24 @@
- import { Link } from "react-router-dom";
-function Home () {
-    return (
-      <div>
-        home page
-          <ul>
-               <li>
-                <Link to="/loginHook">LogIn</Link>
-              </li>
-              <li>
-                <Link to="/SignUp">Sign Up</Link>
-              </li>
-          </ul>
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+import Button from "@material-ui/core/Button";
+
+function Home() {
+  return (
+    <>
+      <div className={styles.divik}>
+        <h1>Home Page</h1>
+        <div>
+          
+          <Button variant="contained" color="primary">
+            <Link to="/login">LogIn</Link>{" "}
+          </Button>
+          
+          <Button variant="contained" color="secondary">
+          <Link to="/SignUp">Sign Up</Link>
+           </Button>
+        </div>
       </div>
-    );
+    </>
+  );
 }
 export default Home;
